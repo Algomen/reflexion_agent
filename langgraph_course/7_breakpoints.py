220db1727c3d08e23c1a78cd58fd1bdc5a62e00c
Alvaro <alvarogomen93@gmail.com>
2025-08-02 21:56:00 +0200

    Added notebooks from course

---
+++ langgraph_course/7_breakpoints.py
@@ -0,0 +1,92 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, SystemMessage, RemoveMessage
+from langgraph.graph import MessagesState, StateGraph, START, END
+from langgraph.prebuilt import ToolNode, tools_condition#
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+from typing import Literal
+import sqlite3
+
+import dotenv
+
+dotenv.load_dotenv()
+
+# In memory
+conn = sqlite3.connect(":memory:", check_same_thread = False)
+db_path = "./example2.db"
+conn = sqlite3.connect(db_path, check_same_thread=False)
+memory = SqliteSaver(conn)
+
+LLM = ChatGoogleGenerativeAI(model="gemini-2.5-pro", temperature=0.0)
+
+# Define a custom state class to include a summary field
+class State(MessagesState):
+    summary: str
+
+# Define the logic to call the model
+def call_model(state: State):
+    # Get summary if it exists
+    summary = state.get("summary", "")
+    # If there is summary, then we add it
+    if summary:
+        # Add summary to system message
+        system_message = f"Summary of conversation earlier: {summary}"
+        # Append summary to any newer messages
+        messages = [SystemMessage(content=system_message)] + state["messages"]
+    else:
+        messages = state["messages"]
+    response = LLM.invoke(messages)
+    return {"messages": response}
+
+
+# Define the summarization function
+def summarize_conversation(state: State):
+    # First, we get any existing summary
+    summary = state.get("summary", "")
+    # Create our summarization prompt 
+    if summary:
+        # A summary already exists
+        summary_message = (
+            f"This is summary of the conversation to date: {summary}\n\n"
+            "Extend the summary by taking into account the new messages above:"
+        )
+    else:
+        summary_message = "Create a summary of the conversation above:"
+    # Add prompt to our history
+    messages = state["messages"] + [HumanMessage(content=summary_message)]
+    response = LLM.invoke(messages)
+    # Delete all but the 2 most recent messages
+    delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-2]]
+    return {"summary": response.content, "messages": delete_messages}
+
+
+# Determine whether to end or summarize the conversation
+def should_continue(state: State) -> Literal ["summarize_conversation",END]:
+    """Return the next node to execute."""
+    messages = state["messages"]
+    # If there are more than six messages, then we summarize the conversation
+    if len(messages) > 6:
+        return "summarize_conversation"
+    # Otherwise we can just end
+    return END
+
+
+
+# Create the state graph
+builder = StateGraph(State)
+builder.add_node("conversation", call_model)
+builder.add_node("summarize_conversation", summarize_conversation)
+builder.add_edge(START, "conversation")
+builder.add_conditional_edges(
+    "conversation",
+    should_continue,
+)
+builder.add_edge("summarize_conversation", END)
+
+graph = builder.compile(checkpointer = memory)
+config = {"configurable": {"thread_id": "1"}}
+
+input_message = HumanMessage(content="That´s cool, what can you help me with")
+output = graph.invoke({"messages": [input_message]}, config) 
+for m in output['messages'][-1:]:
+    m.pretty_print()
