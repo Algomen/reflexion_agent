f7f29b61a6763a3fb70b90f1cce85877e7235e13
Alvaro <alvarogomen93@gmail.com>
2025-08-03 00:33:59 +0200

    human feedback

--- langgraph_course/8_humanFeedback.py
+++ langgraph_course/8_humanFeedback.py
@@ -51,42 +51,54 @@ sys_msg = SystemMessage(
 def assistant(state: MessagesState) -> MessagesState:
     return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
 
+# no-op node that should be interrupted on
+def human_feedback(state: MessagesState):
+    pass
+    # user_input = input("Tell me how you want to update the state: ")
+    # state["messages"].append(HumanMessage(content=user_input))
+    # return state
+
 # Create the state graph
 builder = StateGraph(MessagesState)
+
 builder.add_node("assistant", assistant)
 builder.add_node("tools", ToolNode(tools))
-builder.add_edge(START, "assistant")
+builder.add_node("human_feedback", human_feedback)
+
+builder.add_edge(START, "human_feedback")
+builder.add_edge("human_feedback", "assistant")
 builder.add_conditional_edges(
     "assistant",
     tools_condition,
 )
-builder.add_edge("tools", "assistant")
+builder.add_edge("tools", "human_feedback")
 
 # Use MemorySaver to save the state of the graph
 memory = MemorySaver()
-graph = builder.compile(interrupt_before=["tools"], checkpointer = memory)
+graph = builder.compile(interrupt_before=["human_feedback"], checkpointer = memory)
 
 
 
 # Input
-initial_input = {"messages": HumanMessage(content="Multiply 2 and 3")}
+initial_input = {"messages": "Multiply 2 and 3"}
 
 # Thread
-thread = {"configurable": {"thread_id": "2"}}
+thread = {"configurable": {"thread_id": "5"}}
 
 # Run the graph until the first interruption
 for event in graph.stream(initial_input, thread, stream_mode="values"):
-    event['messages'][-1].pretty_print()
-
-# Get user feedback
-user_approval = input("Do you want to call the tool? (yes/no): ")
-
-# Check approval
-if user_approval.lower() == "yes":
+    event["messages"][-1].pretty_print()
     
-    # If approved, continue the graph execution
-    for event in graph.stream(None, thread, stream_mode="values"):
-        event['messages'][-1].pretty_print()
-        
-else:
-    print("Operation cancelled by user.")
+# # Get user input
+user_input = input("Tell me how you want to update the state: ")
+
+# # We now update the state as if we are the human_feedback node
+graph.update_state(thread, {"messages": user_input}, as_node="human_feedback")
+
+# Continue the graph execution
+for event in graph.stream(None, thread, stream_mode="values"):
+    event["messages"][-1].pretty_print()
+
+# Continue the graph execution
+for event in graph.stream(None, thread, stream_mode="values"):
+    event["messages"][-1].pretty_print()
